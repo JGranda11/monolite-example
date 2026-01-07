ffb50ac6f98db5f9b74bb0fc249fceb9579d9790
Jonathan Granda <jhonatangranda.2002@gmail.com>
2026-01-07 17:36:20 -0500

    implements reports in PDF format

--- src/main/java/com/pragma/gestioncursos/__gestion_cursos/controller/CursoController.java
+++ src/main/java/com/pragma/gestioncursos/__gestion_cursos/controller/CursoController.java
@@ -1,7 +1,9 @@
 package com.pragma.gestioncursos.__gestion_cursos.controller;
 
 import com.pragma.gestioncursos.__gestion_cursos.entity.Curso;
+import com.pragma.gestioncursos.__gestion_cursos.reports.CursoExporterPDF;
 import com.pragma.gestioncursos.__gestion_cursos.repository.CursoRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -79,4 +85,20 @@ public class CursoController {
         return "redirect:/cursos";
     }
 
+    @GetMapping("/export/pdf")
+    public void generarReportePDF(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        String currentDateTime = dateFormat.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=cursos" + currentDateTime +".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        List<Curso> cursos = cursoRepository.findAll();
+
+        CursoExporterPDF exporterPDF = new CursoExporterPDF(cursos);
+
+        exporterPDF.export(response);
+    }
 }
