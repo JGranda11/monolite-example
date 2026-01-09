43a71eb6a8efd5e4f66119edae54b3433c24c3af
Jonathan Granda <jhonatangranda.2002@gmail.com>
2026-01-08 22:31:22 -0500

    implements pagination and searching

--- src/main/java/com/pragma/gestioncursos/__gestion_cursos/controller/CursoController.java
+++ src/main/java/com/pragma/gestioncursos/__gestion_cursos/controller/CursoController.java
@@ -6,16 +6,22 @@ import com.pragma.gestioncursos.__gestion_cursos.reports.CursoExporterPDF;
 import com.pragma.gestioncursos.__gestion_cursos.repository.CursoRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -32,10 +38,31 @@ public class CursoController {
     }
 
     @GetMapping("/cursos")
-    public String listarCursos(Model model){
-        List<Curso> cursos = cursoRepository.findAll();
-        cursos = cursoRepository.findAll();
-        model.addAttribute("cursos", cursos);
+    public String listarCursos(Model model, @Param("keyword") String keyword, @RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "3") int size){ //en este metodo se agrega la paginación
+        try{
+            List<Curso> cursos = new ArrayList<>();
+            //Page-1 por que empezamos en la pagina cero (0)
+            Pageable paging = PageRequest.of(page-1, size);
+
+            Page<Curso> pageCursos = null;
+            if(keyword == null){
+                pageCursos = cursoRepository.findAll(paging);
+            } else {
+                pageCursos = cursoRepository.findByTituloContainingIgnoreCase(keyword, paging);
+                model.addAttribute("keyword", keyword);
+            }
+
+            cursos = pageCursos.getContent();
+            model.addAttribute("cursos", cursos);
+
+            model.addAttribute("currentPage", pageCursos.getNumber() +1);
+            model.addAttribute("totalItems", pageCursos.getTotalElements());
+            model.addAttribute("totalPages", pageCursos.getTotalPages());
+            model.addAttribute("pageSize", size);
+
+        }catch (Exception exception){
+            model.addAttribute("message", exception.getMessage());
+        }
 
         return "cursos";
     }
--- src/main/java/com/pragma/gestioncursos/__gestion_cursos/repository/CursoRepository.java
+++ src/main/java/com/pragma/gestioncursos/__gestion_cursos/repository/CursoRepository.java
@@ -1,13 +1,15 @@
 package com.pragma.gestioncursos.__gestion_cursos.repository;
 
 import com.pragma.gestioncursos.__gestion_cursos.entity.Curso;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CursoRepository extends JpaRepository<Curso, Integer> {
 
     //podemos definir metodos personalizados
-
+    Page<Curso>findByTituloContainingIgnoreCase(String keyword, Pageable pageable);
 }
